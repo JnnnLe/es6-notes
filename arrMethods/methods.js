f487197e41d285c2a00d6de3143083f9afca9d03
JnnnLe <jnnnle.sf@gmail.com>
2018-06-07 17:11:07 -0700

    Method notes

---
+++ arrMethods/methods.js
@@ -0,0 +1,11 @@
+//MAP
+  // does NOT mutate an arr
+
+//FILTER / FIND
+  // needs return statement
+
+  // FIND
+    //finding a single element of the collection
+
+//REDUCE
+  // parameters ((prev, itemOfCollection), valueOfPrev)
